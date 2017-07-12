91eaa845bdc8119463958c080e2fee50cc978212
Samantha Morco <samanthamorco@gmail.com>
2017-07-11 20:50:25 -0400

    add Sami's favorite student

--- lib/go_gitters.rb
+++ lib/go_gitters.rb
@@ -3,6 +3,7 @@ require "go_gitters/version"
 module GoGitters
   class Git
     def random_student
+      puts "This is Sami's favorite student: "
       ["Jordan", "Reilly", "Elvin", "David", "Mamadou", "Avraham"].shuffle[0]
     end
   end
